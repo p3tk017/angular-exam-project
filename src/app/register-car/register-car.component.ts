d42f26799b7a1602e7edf15071e8694b7899a6c4
p3tk017 <petkokunev11182005@gmail.com>
2024-12-11 18:35:47 +0200

    Added register-car form functionality

--- src/app/register-car/register-car.component.ts
+++ src/app/register-car/register-car.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiServiceService } from '../api-service.service';
 
 @Component({
   selector: 'app-register-car',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-car.component.html',
   styleUrl: './register-car.component.css'
 })
 export class RegisterCarComponent {
+  constructor(private apiService: ApiServiceService) {}
 
+  registerCar(form: NgForm) {
+    if(form.invalid) {
+      console.log(form.invalid);
+      return
+    }
+
+    console.log(form.value);
+    
+  }
 }
