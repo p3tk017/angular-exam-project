5638848e0ce07172e3352f631a12f8922672686c
p3tk017 <petkokunev11182005@gmail.com>
2024-12-11 17:31:28 +0200

    Added register form funcionality

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, MinLengthValidator, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { EmailValidator } from '../../utils/email.validator';
+import { DOMAINS } from '../../constants';
+import { matchPasswordsValidator } from '../../utils/match-passwords';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  form = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    email: new FormControl("", [Validators.required, EmailValidator(DOMAINS)]),
+    passGroup: new FormGroup({
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      rePassword: new FormControl("", [Validators.required])
+    },
+    {
+      validators: [matchPasswordsValidator('password', 'rePassword')]
+    }
+  )
+  })
 
+  register() {
+    if(this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+  }
+
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
 }
